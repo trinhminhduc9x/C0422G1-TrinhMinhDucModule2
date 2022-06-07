5404a35c84b8529e77afedc5a85c62ea40f9acd5
trinhminhduc9x <duc1997th@gmail.com>
2022-06-07 16:00:56 +0700

    add

---
+++ src/Ss1_Introductiontojava/baitap/Hienthithoigiancuahethong.java
@@ -0,0 +1,8 @@
+package Ss1_Introductiontojava.baitap;
+import java.util.Date;
+public class Hienthithoigiancuahethong {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(" xin chao moi nguoi bay gio thoi gian la : "+ now);
+    }
+}
